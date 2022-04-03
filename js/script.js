2dd5dc9c08a81d1f74176cc40b9581d7e74e1649
Yoshiaki Numajiri <rea1i2e@icloud.com>
2022-04-03 20:45:31 +0900

    SP表示でのピクセルパーフェクト修正

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  // ローディングアニメーション
+  $(function () {
+    $(window).on('load');
+    $('.loader-slide').addClass('open');
+    setTimeout(function () {
+      $('#loader').css('display', 'none');
+    }, 2000);
+  });
+
   var topBtn = $('.pagetop');
   topBtn.hide();
 
@@ -37,24 +46,52 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   let headNav = $("header");
   let mainVisualHeight = $(".p-mainview").height();
-/* 初期表示、スクロール時にイベント開始 */
-$(window).on("load scroll", function () {
+  /* 初期表示、スクロール時にイベント開始 */
+  $(window).on("load scroll", function () {
     /* スクロール位置の高さが200pxより大きい、かつis-fixedクラスがない場合 */
     if ($(this).scrollTop() > mainVisualHeight && headNav.hasClass("is-fixed") == false) {
-        headNav.css("display", "block");
-        headNav.css({ "top": "-72px" }); /* ヘッダーの高さ分マイナスにする */
-        headNav.addClass("is-fixed");
-        headNav.animate({ "top": 0 }, 600);
-        var changeL = $('.c-logo img').attr('src').replace('logo-w.svg' , 'logo-b.svg');
-        $('.c-logo img').attr('src', changeL);
-      }
-      /* スクロール位置の高さが200pxより小さい、かつis-fixedクラスがある場合 */
-      else if ($(this).scrollTop() < mainVisualHeight && headNav.hasClass("is-fixed") == true) {
-        headNav.removeClass("is-fixed");
-        headNav.css("display", "static");
-        var changeL = $('.c-logo img').attr('src').replace('logo-b.svg' , 'logo-w.svg');
-        $('.c-logo img').attr('src', changeL);
+      headNav.css("display", "block");
+      headNav.css({ "top": "-72px" }); /* ヘッダーの高さ分マイナスにする */
+      headNav.addClass("is-fixed");
+      headNav.animate({ "top": 0 }, 600);
+      var changeL = $('.c-logo img').attr('src').replace('logo-w.svg', 'logo-b.svg');
+      $('.c-logo img').attr('src', changeL);
     }
+    /* スクロール位置の高さが200pxより小さい、かつis-fixedクラスがある場合 */
+    else if ($(this).scrollTop() < mainVisualHeight && headNav.hasClass("is-fixed") == true) {
+      headNav.removeClass("is-fixed");
+      headNav.css("display", "static");
+      var changeL = $('.c-logo img').attr('src').replace('logo-b.svg', 'logo-w.svg');
+      $('.c-logo img').attr('src', changeL);
+    }
+  });
 });
 
+// AOS.js
+AOS.init({
+  offset: 100,
+  // アニメーションの開始位置（トリガーの元の位置より100px下に設定）
+  // offset (in px) from the original trigger point    
+
+  delay: 0,
+  // 遅延 values from 0 to 3000, with step 50ms
+
+  duration: 1000,
+  // アニメーションにかける時間（1000ミリ秒かける）
+  // values from 0 to 3000, with step 50ms
+
+  easing: 'ease',
+  // アニメーションの仕方　default easing for AOS animations
+
+  once: true,
+  // trueにすると、1回だけ実行され、falseだとずっと実行される
+  // whether animation should happen only once - while scrolling down
+
+  mirror: false,
+  // 要素をスクロールしながら、アニメーション化する必要があるかどうか
+  // whether elements should animate out while scrolling past them
+
+  anchorPlacement: 'top-bottom',
+  // アニメーション開始のスクロール位置
+  // defines which position of the element regarding to window should trigger the animation
 });
